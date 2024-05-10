b7f4e220a452de5f3cb4dc5a2ca0a409517e06ef
erjanbapyev <bapyeverzhan@gmail.com>
2024-05-10 12:25:40 +0600

    hw2-3

--- config.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Bot, Dispatcher
-from decouple import config
-
-TOKEN = config('TOKEN')
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-ADMIN_ID=config('ADMIN_ID')
--- database/sql_queries.py
+++
@@ -1,8 +0,0 @@
-CREATE_USER_TABLE_QUERY = '''CREATE TABLE IF NOT EXISTS telegramm_users(
-id INTEGER PRIMARY KEY,
-first_name TEXT,
-tg_id INTEGER,
-UNIQUE(tg_id))'''
-INSERT_USER = '''INSERT OR IGNORE INTO telegramm_users VALUES (?, ?, ?)'''
-
-SELECT_USER = '''SELECT * FROM telegramm_users'''
